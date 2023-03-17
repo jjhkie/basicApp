7366056d3ccdc7b7d094921a7595725c547e0eb7
jjhkie <83710866+jjhkie@users.noreply.github.com>
2023-03-17 14:56:07 +0900

    Initial Commit

---
+++ basicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  basicApp
+//
+//  Created by 김진혁 on 2023/03/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
